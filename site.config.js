e34d02550309120534c71d52ffb6bc296f15d353
DahyeonKang <kl1223k84@gmail.com>
2021-06-27 01:14:49 +0900

    #2 : change names
    * change pageid, base site, metadata, username

--- site.config.js
+++ site.config.js
@@ -1,25 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '58c689e271e24598a23a69185fc27e0c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Dahyeon Kang',
+  domain: 'dahyeon-kang',
+  author: 'Dahyeon Kang',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'write',
+  socialImageTitle: 'data analysis',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'transitive_bs',
+  github: 'DahyeonKang',
+  //linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -32,6 +31,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
+  //utterancesGitHubRepo: 'DahyeonKang/DahyeonKang.github.io',
   utterancesGitHubRepo: null,
 
   // whether or not to enable support for LQIP preview images (optional)
