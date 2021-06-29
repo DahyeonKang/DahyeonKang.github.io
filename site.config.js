9fc1531468f6c7c230fc39ea6d374a4fb5e18f55
DahyeonKang <69884205+DahyeonKang@users.noreply.github.com>
2021-06-29 20:59:17 +0900

    #2: change from other to myinfo
    * PageId
    * basic site
    * social usernames

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
   // social usernames (optional)
   //twitter: 'transitive_bs',
   github: 'DahyeonKang',
-  //linkedin: 'fisch2',
+  linkedin: 'Dahyeon K',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
